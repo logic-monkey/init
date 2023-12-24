1618b263370af7bdea5f682839437e47140b8a89
Logic Monkey <logic.monkey@protonmail.com>
2023-12-24 04:11:10 -0600

    I can't remember what I did here.

--- intializer.gd
+++ intializer.gd
@@ -1,12 +1,19 @@
 extends Node
-
+## The Initializer is simply a autoload script, accessed with _INIT, that has a dictionary,
+## _INIT.data, which carries initialization data. It loads this data at the start of the
+## program and saves it when the program quits, to an easily readible txt file for the
+## gamer to modify at will. It should be used to save user options like keyboard layouts
+## or volume settings. It should NOT be used for things like save games, or anything that
+## ought to be resistant to hacking.
+ 
+## The dictionary that gets converted to and from a text file on game start.
 var data = {}
-var NAME = ProjectSettings["application/config/name"]
-var FILE = "user://%s init.txt" % NAME
+var _NAME = ProjectSettings["application/config/name"]
+var _FILE = "user://%s init.txt" % _NAME
 
-var POSITION = "window position"
-var SIZE = "window size"
-var MODE = "window mode"
+const POSITION = "window position"
+const SIZE = "window size"
+const MODE = "window mode"
 
 func Save():
 	data[MODE] = DisplayServer.window_get_mode()
@@ -14,7 +21,7 @@ func Save():
 		data[SIZE] = DisplayServer.window_get_size()
 		data[POSITION] = DisplayServer.window_get_position()
 	
-	var file = FileAccess.open(FILE,FileAccess.WRITE)
+	var file = FileAccess.open(_FILE,FileAccess.WRITE)
 	file.store_string(var_to_str(data))
 	
 
@@ -23,12 +30,12 @@ var is_loaded := false
 
 func Load():
 	is_loaded = false
-	if not FileAccess.file_exists(FILE): 
+	if not FileAccess.file_exists(_FILE): 
 		Save()
 		is_loaded = true
 		emit_signal("loaded")
 		return
-	var sample_data = str_to_var(FileAccess.get_file_as_string(FILE))
+	var sample_data = str_to_var(FileAccess.get_file_as_string(_FILE))
 	if sample_data is Dictionary: data = sample_data
 	else : 
 		is_loaded = true
